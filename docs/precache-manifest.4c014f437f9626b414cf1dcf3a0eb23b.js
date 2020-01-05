32d6f1bceeaa5eab451b58ca95526a5523904da8
Alen Thomas <alenthomas9@gmail.com>
2020-01-06 02:56:55 +0530

    Add readme and docs

---
+++ docs/precache-manifest.4c014f437f9626b414cf1dcf3a0eb23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165a9ea8a4fdd81023036781e7e1805a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cfd459ae7746abb5eba",
+    "url": "/static/css/main.83aa8550.chunk.css"
+  },
+  {
+    "revision": "c1c2620a84dfdc0fd497",
+    "url": "/static/js/2.28e252bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.28e252bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cfd459ae7746abb5eba",
+    "url": "/static/js/main.83862584.chunk.js"
+  },
+  {
+    "revision": "26b7986e1d314c7a183c",
+    "url": "/static/js/runtime-main.6ab484e7.js"
+  }
+]);
